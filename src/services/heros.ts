c632ebed90dbf038f0ff229130a0eafee0941a27
Camila Jullyane <camilaj.silva0@gmail.com>
2024-08-15 10:32:06 -0300

    fix: api keys

--- src/services/heros.ts
+++ src/services/heros.ts
@@ -3,8 +3,9 @@ import { api } from "../api/api";
 import { AxiosPromise } from "axios";
 import { apiResponseModel } from "../models/apiResponseModel";
 
-const publicKey = "78a792eecc864e97f2e1a9ce44c85f2c";
-const privateKey = "2be91eb5bd95847c3a728c94824c55550436a662";
+const publicKey = process.env.REACT_APP_PUBLIC_KEY as string;
+
+const privateKey = process.env.REACT_APP_PRIVATE_KEY as string;
 const time = Number(new Date());
 const hash = md5(time + privateKey + publicKey);
 
